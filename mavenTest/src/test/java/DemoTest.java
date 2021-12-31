c9b15485fad360dfdc8c51c0c45e852d5f93cace
anils <anils@AnilSaner>
2021-12-31 14:47:05 +0530

    mvn commit

---
+++ mavenTest/src/test/java/DemoTest.java
@@ -0,0 +1,19 @@
+
+
+
+
+
+public class DemoTest {
+
+	
+	public void createContactTest() {
+		
+		System.out.println("Contact has been created");
+	}
+	
+	
+	public void createOrgTest() {
+		System.out.println("fsdgfg");
+		//Reporter.log("Org has been created",true);
+ }
+}

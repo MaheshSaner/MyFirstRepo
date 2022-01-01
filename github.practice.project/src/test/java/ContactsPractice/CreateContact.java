f79dbea012f550074563ea164005f4dfb5a35bec
anils <anils@AnilSaner>
2022-01-01 21:08:43 +0530

    my first commit

---
+++ github.practice.project/src/test/java/ContactsPractice/CreateContact.java
@@ -0,0 +1,19 @@
+package ContactsPractice;
+
+import org.testng.annotations.Test;
+
+public class CreateContact {
+	
+	@Test
+	
+	public void createContactTest() {
+		System.out.println("Contact Created");
+	}
+	
+	@Test
+	
+	public void updateContact() {
+		System.out.println("Contact updated");
+	}
+
+}
---
+++ github.practice.project/src/test/java/demoTest/OrgTest.java
@@ -0,0 +1,17 @@
+package demoTest;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	
+	public void createOrgTest() {
+		System.out.println("Org has been created");
+	}
+	
+	@Test
+	public void updateOrg() {
+		System.out.println("Org has been updated");
+	}
+}
